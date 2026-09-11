0ea58fa56db54661ed94239c0f17fe56af7a0764
Yii-de <totuanhuy24082005@gmail.com>
2026-09-11 11:08:25 +0700

    Initial commit

---
+++ src/main/java/com/example/bookrunner/BookRunnerApplication.java
@@ -0,0 +1,13 @@
+package com.example.bookrunner;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BookRunnerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BookRunnerApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/bookrunner/BookRunnerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.bookrunner;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BookRunnerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
